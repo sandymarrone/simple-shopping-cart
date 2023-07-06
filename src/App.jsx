6bb7266bcd3de7b0cf1e116a8a232e9b9ee09832
Samcastillo <sandyamc10@gmail.com>
2023-07-06 12:45:21 -0400

    finished app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react"
 import { Header } from "./components/Header"
+import { ProductList } from "./components/ProductList"
 
 function App() {
+  const [allProducts, setAllProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
 
   return (
     <>
-      <Header />
+      <Header 
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+      <ProductList 
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+       />
     </>
   )
 }
